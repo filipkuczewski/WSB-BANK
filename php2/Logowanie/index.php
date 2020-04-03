336cc2f99c6f1587bc1497786118ab70cb37a3ed
Filip Kuczewski <filipkuczewski@gmail.com>
2020-04-03 16:58:13 +0200

    Logowanie Nieukonczone

---
+++ php2/Logowanie/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osadnicy - gra przeglądarkowa</title>
+</head>
+<body>
+
+Tylko martwi ujrzeli koniec wojny - Platon </br></br>
+
+
+<form action="zaloguj.php" method="post">
+
+Login: <br/><input type="text" name="login"/><br/>
+Hasło: <br/><input type="password" name="haslo"/><br/>
+<input type="submit" value="Zaloguj się">
+
+</form>
+
+</body>
+</html>
---
+++ php2/Logowanie/zaloguj.php
@@ -0,0 +1,27 @@
+<?php
+
+
+require_once "connect.php";
+
+$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+
+if($polaczenie->connect_errno != 0){
+    echo "Error".$polaczenie->connect_errno ."Opis: ".$polaczenie->connect_error;  
+}
+else{
+    $login = $_POST['login'];
+    $haslo = $_POST['haslo'];
+
+    echo "It works";
+
+    $polaczenie->close();
+}
+
+
+
+echo $login."<br/>";
+echo $haslo."<br/>";
+
+
+
+?>
