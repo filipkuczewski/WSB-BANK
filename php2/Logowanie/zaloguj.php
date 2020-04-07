266fb375df51cbcc62d7e78eb13d8ce6ad49add7
Filip Kuczewski <filipkuczewski@gmail.com>
2020-04-07 15:52:29 +0200

    complete login with and noproblem with sqlinject

--- php2/Logowanie/zaloguj.php
+++ php2/Logowanie/zaloguj.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if((!isset($_POST['login'])) || (!isset($_POST['haslo']))){
+if ((!isset($_POST['login'])) || (!isset($_POST['haslo']))) {
     header('Location: index.php');
     exit();
 }
@@ -11,22 +11,26 @@ require_once "connect.php";
 
 $polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
 
-if($polaczenie->connect_errno != 0){
-    echo "Error".$polaczenie->connect_errno;  
-}
-else{
+if ($polaczenie->connect_errno != 0) {
+    echo "Error" . $polaczenie->connect_errno;
+} else {
     $login = $_POST['login'];
     $haslo = $_POST['haslo'];
 
+    $login = htmlentities($login, ENT_QUOTES, "UTF-8");
+    $haslo = htmlentities($haslo, ENT_QUOTES, "UTF-8");
 
-    $sql = "Select * from uzytkownicy where user='$login' and pass='$haslo'";
     //if by w razie zapytania ktore zle jest skonstruowane w zmiennej $sql
-    if($rezultat = @$polaczenie->query($sql)){
-        $ilu_userow = $rezultat->num_rows;;
-        if($ilu_userow>0){
+    if ($rezultat = @$polaczenie->query(
+        sprintf(
+            "Select * from uzytkownicy where user='%s' and pass='%s'",
+            mysqli_real_escape_string($polaczenie, $login),
+            mysqli_real_escape_string($polaczenie, $haslo)))) {
+        $ilu_userow = $rezultat->num_rows;
+        if ($ilu_userow > 0) {
 
             $_SESSION['zalogowany'] = true;
-            
+
             $wiersz = ($rezultat)->fetch_assoc();
             $_SESSION['id'] = $wiersz['id'];
             $_SESSION['user'] = $wiersz['user'];
@@ -36,22 +40,18 @@ else{
             $_SESSION['email'] = $wiersz['email'];
             $_SESSION['dnipremium'] = $wiersz['dnipremium'];
 
-            
+
 
             unset($_SESSION['blad']);
             $rezultat->free_result();
             header('Location: gra.php');
-
-        }else{
+        } else {
             $_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło</span>';
             header('Location: index.php');
         }
-       
-    } else{
+    } else {
         echo "Błędna konstrukcja zapytania sql";
     }
 
     $polaczenie->close();
 }
-
-?>
